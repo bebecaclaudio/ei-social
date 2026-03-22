47dbdbeb73c65e2f81faf9fc27bceeddda79bb63
Sofia Rebeca <sofiarebeca11@hotmail.com>
2026-03-22 16:25:33 -0300

    Corrigindo config e exportando banco

--- ei-social/src/firebase-config.js
+++ ei-social/src/firebase-config.js
@@ -1,23 +1,22 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDM-lK7SVxnQ8j2_QacTPLauE50RaTKPRM",
-    authDomain: "ei-social.firebaseapp.com",
-    projectId: "ei-social",
-    storageBucket: "ei-social.firebasestorage.app",
-    messagingSenderId: "1005094792930",
-    appId: "1:1005094792930:web:9519944aabba7cf8c778bb",
-    measurementId: "G-8QSJC6HDTQ"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDM-lK7SVxnQ8j2_QacTPLauE50RaTKPRM",
+  authDomain: "ei-social.firebaseapp.com",
+  projectId: "ei-social",
+  storageBucket: "ei-social.firebasestorage.app",
+  messagingSenderId: "1005094792930",
+  appId: "1:1005094792930:web:9519944aabba7fc8c778bb",
+  measurementId: "G-8QSJC6HDTQ"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exporta as ferramentas para usar no Feed.jsx, Cadastro.jsx, etc.
+export const db = getFirestore(app);
+export const auth = getAuth(app);
